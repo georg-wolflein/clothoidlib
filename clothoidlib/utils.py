Fix broadcasting semantics

clothoidlib/utils.py
@@ -24,7 +24,7 @@ def angle_between(v1: np.ndarray, v2: np.ndarray) -> np.ndarray:
     v2 /= np.linalg.norm(v2, axis=-1, keepdims=True)
     v1 = np.expand_dims(v1, axis=-2)
     v2 = np.expand_dims(v2, axis=-1)
-    return np.arccos(np.clip(np.matmul(v1, v2), -1., 1.)).reshape(v1.shape[:-2])
+    return np.arccos(np.clip(np.matmul(v1, v2), -1., 1.)).squeeze(axis=(-1, -2))
 
 
 class ChangeOfBasis: