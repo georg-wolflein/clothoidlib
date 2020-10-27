e9995b81c3b6617618190a4191a516ac4c1991ee
Georg Wölflein <georgw7777@gmail.com>
2020-10-27 15:42:53 +0000

    Implement get_clothoid_point_at_angle

--- clothoidlib/calculator.py
+++ clothoidlib/calculator.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 from scipy.spatial import KDTree
+from scipy.signal import argrelmin
 from sklearn.metrics.pairwise import euclidean_distances
 import typing
 import functools
@@ -25,75 +26,76 @@ class ClothoidParameters(typing.NamedTuple):
     lambda_c: np.ndarray
 
 
-def compute_clothoid_table(t_samples: np.ndarray) -> ClothoidParameters:
-    """Calculate the clothoid parameter table for a given set of samples for t.
-
-    Args:
-        t_samples (np.ndarray): the samples
-
-    Returns:
-        ClothoidParameters: the computed clothoid parameters
-    """
-
-    # Create a matrix of all possible combinations of t1 and t2 values
-    t1, t2 = np.stack(np.meshgrid(t_samples, t_samples),
-                      axis=-1).reshape(-1, 2).T
-
-    # Discard the rows where t1 < t2
-    mask = (t1 < t2) & (t1 > 0)
-    t1 = t1[mask]
-    t2 = t2[mask]
-
-    # Calculate points
-    ts = np.stack((np.zeros_like(t1), t1, t2), axis=0)
-    p0, p1, p2 = np.stack(fresnel(ts), axis=-1)
-
-    # Calculate angles
-    gamma1 = angle_between(p1-p0, p1-p2)
-    gamma2 = angle_between(p2-p0, p2-p1)
-    theta = np.pi * t2**2 / 2  # angle at end
-    omega = np.arctan(p1[..., 1] / p1[..., 0])
-    beta = omega + np.pi - gamma1 - gamma2
-    alpha = theta - beta
-
-    # Calculate t0
-    lengths = np.linalg.norm(p2 - p1, axis=-1)
-    t0 = np.zeros_like(t1)
-    for current_t1 in t_samples:
-        t1_mask = t1 == current_t1
-        t2_mask = t2 == current_t1
-        t1_lengths = lengths[t1_mask]
-        t2_lengths = lengths[t2_mask]
-        if t1_mask.sum() == 0 or t2_mask.sum() == 0:
-            continue
-        distance_matrix = euclidean_distances(
-            t1_lengths[:, np.newaxis], t2_lengths[:, np.newaxis])
-        t2_masked_indices = np.argmin(distance_matrix, axis=1)
-        t2_indices = np.arange(len(t2))[t2_mask][t2_masked_indices]
-        associated_t1_values = t1[t2_indices]
-        t0[t1_mask] = associated_t1_values
-
-    # Calculate lambdas
-    subgoals = np.stack(fresnel(t0), axis=-1)
-    lambdas = ChangeOfBasis(p1, p2)(subgoals)
-
-    return ClothoidParameters(gamma1, gamma2, alpha, beta, t0, t1, t2, *lambdas.T)
-
-
 class ClothoidCalculator:
     """Fast and efficient computation of clothoids.
     """
 
     def __init__(self, samples: float = 1000, t_max: float = np.sqrt(3)):
-        t_samples = np.linspace(0, t_max, samples)
+        self._t_samples = np.linspace(0, t_max, samples)
+        self._point_samples = np.stack(fresnel(self._t_samples), axis=-1)
 
         # Calculate clothoid parameters
-        gamma1, gamma2, *values = compute_clothoid_table(t_samples)
+        gamma1, gamma2, *values = self.compute_clothoid_table(self._t_samples)
 
         # Construct kd-tree
         indices = np.array((gamma1, gamma2)).T
         self._values = np.array(values).T
         self._tree = KDTree(indices)
+
+    @classmethod
+    def compute_clothoid_table(cls, t_samples: np.ndarray) -> ClothoidParameters:
+        """Calculate the clothoid parameter table for a given set of samples for t.
+
+        Args:
+            t_samples (np.ndarray): the samples
+
+        Returns:
+            ClothoidParameters: the computed clothoid parameters
+        """
+
+        # Create a matrix of all possible combinations of t1 and t2 values
+        t1, t2 = np.stack(np.meshgrid(t_samples, t_samples),
+                          axis=-1).reshape(-1, 2).T
+
+        # Discard the rows where t1 < t2
+        mask = (t1 < t2) & (t1 > 0)
+        t1 = t1[mask]
+        t2 = t2[mask]
+
+        # Calculate points
+        ts = np.stack((np.zeros_like(t1), t1, t2), axis=0)
+        p0, p1, p2 = np.stack(fresnel(ts), axis=-1)
+
+        # Calculate angles
+        gamma1 = angle_between(p1-p0, p1-p2)
+        gamma2 = angle_between(p2-p0, p2-p1)
+        theta = np.pi * t2**2 / 2  # angle at end
+        omega = np.arctan(p1[..., 1] / p1[..., 0])
+        beta = omega + np.pi - gamma1 - gamma2
+        alpha = theta - beta
+
+        # Calculate t0
+        lengths = np.linalg.norm(p2 - p1, axis=-1)
+        t0 = np.zeros_like(t1)
+        for current_t1 in t_samples:
+            t1_mask = t1 == current_t1
+            t2_mask = t2 == current_t1
+            t1_lengths = lengths[t1_mask]
+            t2_lengths = lengths[t2_mask]
+            if t1_mask.sum() == 0 or t2_mask.sum() == 0:
+                continue
+            distance_matrix = euclidean_distances(
+                t1_lengths[:, np.newaxis], t2_lengths[:, np.newaxis])
+            t2_masked_indices = np.argmin(distance_matrix, axis=1)
+            t2_indices = np.arange(len(t2))[t2_mask][t2_masked_indices]
+            associated_t1_values = t1[t2_indices]
+            t0[t1_mask] = associated_t1_values
+
+        # Calculate lambdas
+        subgoals = np.stack(fresnel(t0), axis=-1)
+        lambdas = ChangeOfBasis(p1, p2)(subgoals)
+
+        return ClothoidParameters(gamma1, gamma2, alpha, beta, t0, t1, t2, *lambdas.T)
 
     def lookup_angles(self, gamma1: np.ndarray, gamma2: np.ndarray) -> ClothoidParameters:
         """Lookup clothoid parameters by providing the values of gamma1 and gamma2. 
@@ -188,38 +190,44 @@ class ClothoidCalculator:
         A = compute_transformation_matrix(C, P)
         return affine_transform(np.array(fresnel(np.linspace(0, t2, n_samples))).T, A)
 
-    def get_clothoid_point_at_angle(self, params: ClothoidParameters, angle: float, n_samples: int = 200) -> typing.Tuple[np.ndarray, np.ndarray]:
+    def get_clothoid_point_at_angle(self, params: ClothoidParameters, angle: float) -> typing.Tuple[np.ndarray, np.ndarray]:
         """Get the point on the clothoid that intersects a line drawn from the start point at a specific angle to the goal line.
 
         Args:
             params (ClothoidParameters): the clothoid parameters
-            angle (float): the angle with the goal line
-            n_samples (int, optional): the number of samples to use. Defaults to 200.
+            angle (np.ndarray): the angle with the goal line
 
         Returns:
             typing.Tuple[np.ndarray, np.ndarray]: a tuple of (t, point)
         """
 
-        t_samples = np.linspace(0, params.t2, n_samples)
+        n_samples = np.argmax(self._t_samples > params.t2)
+        t_samples = self._t_samples[:n_samples]
+        point_samples = self._point_samples[:n_samples]
 
-        # remove last few samples because we don't want to consider the start point (which by definition lies on the clothoid)
-        t_samples = t_samples[:-np.ceil(n_samples * .01).astype(np.int)]
-        samples_x, samples_y = fresnel(t_samples)
+        samples_x, samples_y = point_samples.T
 
         # the line is given by y = mx + b
         angle_with_x_axis = params.beta + angle
-        print(angle_with_x_axis)
         m = np.tan(angle_with_x_axis)
 
         # calculate b using the fact that the line must pass through the current point: y = mx + b <=> b = y - mx
         current_point = x, y = fresnel(params.t2)
         b = y - m * x
 
+        # If angle > alpha, we are certain that the closest point is the current point
+        if not (0 < angle < params.alpha):
+            return params.t2, current_point
+
+        # Calculate distance of each point on clothoid to the line
         def distance_to_line(x, y):
             return np.abs(y - m*x - b) / np.sqrt(m**2 + 1)
 
         distances = distance_to_line(samples_x, samples_y)
-        closest_point_index = np.argmin(distances, axis=-1)
 
-        samples = np.array([samples_x, samples_y]).T
-        return t_samples[closest_point_index], samples[closest_point_index]
+        # Find local minima in distances
+        local_minima = argrelmin(distances, axis=-1)
+        closest_point_index = local_minima[0] if len(
+            local_minima) > 0 else np.argmin(distances, axis=-1)
+
+        return t_samples[closest_point_index], point_samples[closest_point_index]
